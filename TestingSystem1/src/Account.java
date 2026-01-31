7e4f81e4fad5c480ed4716bbf2c4db545b2a260c
Mai Linh <maiiilinhhh220204@gmai.com>
2026-01-31 09:28:37 +0700

    remove kkkk

---
+++ TestingSystem1/src/Account.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Account {
+    String fullname;
+    Gender gender;
+    Date CreateDate;
+    int age;
+}
---
+++ TestingSystem1/src/Hotel.java
@@ -0,0 +1,3 @@
+public class Hotel { //object
+    String title; //attribute
+}
